ad4165fee75aa129ad3855c4295e7106f593d5a3
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2021-08-02 09:21:52 +0200

    adding aws_region var in vpc module

--- terraform/aws/module-infra/variables.tf
+++ terraform/aws/module-infra/variables.tf
@@ -3,6 +3,12 @@ variable "customer" {}
 variable "env" {}
 variable "project" {}
 
+# AWS
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "eu-west-1"
+}
+
 #
 # VPC
 #

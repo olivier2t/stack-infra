75cf5c77e16833522878492146af7eed3a8354b0
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2021-08-04 08:51:55 +0200

    change custom_keypair

--- terraform/aws/module-infra/variables.tf
+++ terraform/aws/module-infra/variables.tf
@@ -50,6 +50,7 @@ variable "bastion_instance_type" {
 
 variable "keypair_public" {
   description = "The public SSH key, for SSH access to newly-created instances"
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
 }
 
 variable "extra_tags" {

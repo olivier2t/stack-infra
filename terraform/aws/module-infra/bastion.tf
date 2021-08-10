8ea5e9dfd73f5029c979f1d1175f136874b43291
Olivier de Turckheim <olivier.deturckheim@cycloid.io>
2021-08-10 09:26:09 +0200

    add env in resource names

--- terraform/aws/module-infra/bastion.tf
+++ terraform/aws/module-infra/bastion.tf
@@ -1,9 +1,9 @@
 resource "aws_security_group" "bastion" {
   count = var.bastion_count > 0 ? 1 : 0
 
-  name        = "bastion"
+  name = "${var.customer}-${var.env}-bastion"
   description = "Allow SSH traffic from the internet to bastion"
-  vpc_id      = module.infra_vpc.vpc_id
+  vpc_id = module.infra_vpc.vpc_id
 
   ingress {
     from_port   = 22
@@ -20,7 +20,7 @@ resource "aws_security_group" "bastion" {
   }
 
   tags = merge(local.merged_tags, {
-    Name       = "bastion"
+    Name = "${var.customer}-${var.env}-bastion"
   })
 }
 
@@ -28,9 +28,9 @@ resource "aws_security_group" "bastion" {
 resource "aws_security_group" "allow_bastion_infra" {
   count = var.bastion_count > 0 ? 1 : 0
 
-  name        = "allow-bastion-infra"
+  name = "${var.customer}-${var.env}-allow-bastion-infra"
   description = "Allow SSH traffic from the bastion to the infra"
-  vpc_id      = module.infra_vpc.vpc_id
+  vpc_id = module.infra_vpc.vpc_id
 
   ingress {
     from_port       = 22
@@ -41,48 +41,48 @@ resource "aws_security_group" "allow_bastion_infra" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
   }
 
   tags = merge(local.merged_tags, {
-    Name       = "allow-bastion-infra"
+    Name = "${var.customer}-${var.env}-allow-bastion-infra"
   })
+}
+
+resource "aws_key_pair" "bastion" {
+  key_name = "${var.customer}-${var.env}-bastion-key"
+  public_key = var.keypair_public
 }
 
 resource "aws_eip" "bastion" {
   count = var.bastion_count
 
-  instance = aws_instance.bastion[0].id
-  vpc      = true
+  instance = aws_instance.bastion[count.index].id
+  vpc = true
 
   tags = merge(local.merged_tags, {
-    Name       = "${var.customer}-bastion${count.index}"
+    Name = "${var.customer}-${var.env}-bastion-${count.index}"
   })
-}
-
-resource "aws_key_pair" "bastion" {
-  key_name   = "bastion-key"
-  public_key = var.keypair_public
 }
 
 resource "aws_instance" "bastion" {
   count = var.bastion_count
 
-  ami           = data.aws_ami.debian.id
+  ami = data.aws_ami.debian.id
   instance_type = var.bastion_instance_type
-  key_name      = aws_key_pair.bastion.key_name
+  key_name = aws_key_pair.bastion.key_name
 
   vpc_security_group_ids = [aws_security_group.bastion[0].id]
 
-  subnet_id               = element(module.infra_vpc.public_subnets, count.index)
+  subnet_id = element(module.infra_vpc.public_subnets, 0)
   disable_api_termination = false
 
   tags = merge(local.merged_tags, {
-    Name       = "${var.customer}-bastion${count.index}"
-    role       = "bastion"
+    Name = "${var.customer}-${var.env}-bastion-${count.index}"
+    role = "bastion"
   })
 
   lifecycle {
--- terraform/aws/module-infra/vpc.tf
+++ terraform/aws/module-infra/vpc.tf
@@ -1,13 +1,13 @@
 module "infra_vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
+  source = "terraform-aws-modules/vpc/aws"
   version = "~> v3.0.0"
 
-  name = "${var.customer}-${var.project}-${var.env}"
+  name = "${var.customer}-${var.env}-vpc"
   cidr = var.cidr
-  azs  = ["${var.aws_region}a"]
+  azs = ["${var.aws_region}a"]
 
-  public_subnets     = var.public_subnets
-  private_subnets    = var.private_subnets
+  public_subnets = var.public_subnets
+  private_subnets = var.private_subnets
   
   single_nat_gateway = true
   enable_nat_gateway = true
